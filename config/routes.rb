1678ee65ae25f07ec2eaa228d39ac1f1c8e3dd69
fnorton <fillipenorton@gmail.com>
2013-05-20 09:03:27 -0300

    Customize url index do blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ::Refinery::Application.routes.draw do
-  scope(:path => 'blog', :module => 'blog') do
+  scope(:path => 'blog/sistema-locadora-veiculo', :module => 'blog') do
     root :to => 'posts#index', :as => 'blog_root'
     match 'feed.rss', :to => 'posts#index', :as => 'blog_rss_feed', :defaults => {:format => "rss"}
     match ':id', :to => 'posts#show', :as => 'blog_post'
